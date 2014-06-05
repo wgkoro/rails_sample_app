14fb550938d5bb6b17a6bb713619690c796f4a1f
Matsukizono <matsukiono@photocreate.co.jp>
2014-06-05 15:57:24 +0900

    End of chapter 7.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,10 +31,10 @@ describe "UserPages" do
 
     describe "with valid information" do
       before do
-        fill_in "Name", with:"Example User"
-        fill_in "Email", with:"user@example.com"
-        fill_in "Password", with:"foobar"
-        fill_in "Confirmation", with:"foobar"
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
       end
 
       it "should create a user" do
