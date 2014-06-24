58e58eb76dad8568e04aab5194ffca09d7465d63
wgkoro <zeathwing@gmail.com>
2014-06-24 21:25:40 +0900

    end of 9.2.2 ただしもう一回読む

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,7 +22,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -35,6 +37,15 @@ class UsersController < ApplicationController
   private 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 
 end
