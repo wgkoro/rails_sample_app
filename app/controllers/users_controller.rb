f84032c6d8f16418b018273c05ffa22ba6abff8c
wgkoro <zeathwing@gmail.com>
2014-06-30 01:28:48 +0900

    end of 10.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,11 +51,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,8 +52,8 @@ describe "User Pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    let(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
-    let(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
     before { visit user_path(user) }
 
@@ -63,7 +63,7 @@ describe "User Pages" do
     describe "microposts" do
       it { should have_content(m1.content) }
       it { should have_content(m2.content) }
-      it { should have_content(user.microposts.cont) }
+      it { should have_content(user.microposts.count) }
     end
   end
 
