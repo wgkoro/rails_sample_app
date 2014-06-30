dc3f152c24ee7723f0c6ae85a0b33b9bbb2bb73f
wgkoro <zeathwing@gmail.com>
2014-06-30 11:50:05 +0900

    end of 10.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -27,12 +27,19 @@ describe "Static pages" do
   end
 
   describe "Home page" do
-    before { visit root_path }
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Loren ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
+    end
 
-    let(:heading) {'Sample App'}
-    let(:page_title) {'Home'}
-
-    it_should_behave_like "all static pages"
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        expect(page).to have_selector("li##{item.id}", text: item.content)
+      end
+    end
   end
 
   describe "Help page" do
